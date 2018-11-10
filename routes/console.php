f515df0a75554f7b1959ecfd5ee87219bc0dea5d
Иван Недзвецкий <stels-cs@yandex.ru>
2018-11-10 22:16:49 +0300

    Calendar demo

--- routes/console.php
+++ routes/console.php
@@ -13,6 +13,23 @@ use Illuminate\Foundation\Inspiring;
 |
 */
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
+Artisan::command('test-notification', function () {
+
+    $accessToken = 'de3b7b53de3b7b53de3b7b533fde5d8fd4dde3bde3b7b5385dd6d3151ee7b85043eae39';
+
+    $res = \Vk\Executor::api("notifications.sendMessage", [
+        "user_ids" => implode(",", [19039187]),
+        "message" => "Hello world",
+        "fragment" => "test",
+        "v" => "5.87",
+        "access_token" => $accessToken
+    ]);
+
+    if ($res->isSuccess()) {
+        $this->info($res->getRawResponse());
+    } else {
+        $this->info($res->getCode().' '.$res->getMessage());
+    }
 })->describe('Display an inspiring quote');
+
+
