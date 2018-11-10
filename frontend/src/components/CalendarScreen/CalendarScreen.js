b03dc3e6f3f070f5fff82905f18e219c6cdfc346
Иван Недзвецкий <stels-cs@yandex.ru>
2018-11-11 02:02:48 +0300

    Fixes

--- frontend/src/components/CalendarScreen/CalendarScreen.js
+++ frontend/src/components/CalendarScreen/CalendarScreen.js
@@ -4,11 +4,21 @@ import "./CalendarScreen.css"
 import Calendar from "../Calendar/Calendar"
 import moment from "moment"
 import L from "../../lang/L"
+import {GetDayOfCycle} from "../../tools/helpers"
 
 class CalendarScreen extends Component {
 
 	render() {
-		return <div className="CalendarScreen">
+		const {cycleLength, menstruatedAt, menstruationLength, selectedDate} = this.props
+
+		const dayOfCycle = GetDayOfCycle(cycleLength, menstruatedAt.timestampMs(), selectedDate)
+		const isBadDay = dayOfCycle <= menstruationLength
+
+		let classList = ["CalendarScreen"]
+		if (!isBadDay) {
+			classList.push("CalendarScreen--green")
+		}
+		return <div className={classList.join(' ')}>
 			<div className="CalendarScreen__week-day-list">
 				<div className="CalendarScreen__week-day">{L.t('week_day_short_1')}</div>
 				<div className="CalendarScreen__week-day">{L.t('week_day_short_2')}</div>
@@ -27,7 +37,10 @@ class CalendarScreen extends Component {
 
 function map(state) {
 	return {
-
+		cycleLength: state.UserModule.cycleLength || 28,
+		menstruatedAt: state.UserModule.menstruatedAt,
+		menstruationLength: state.UserModule.menstruationLength,
+		selectedDate: state.UserModule.selectedDate,
 	}
 }
 
