90971e611a16b1b0b2f7b14e72e6b4e6446869d9
Dmitry Pantyukhov <dmitrypantyukhov@gmail.com>
2018-11-11 01:14:48 +0300

    text fix

--- frontend/src/lang/locales/ru.js
+++ frontend/src/lang/locales/ru.js
@@ -4,7 +4,7 @@ export default function ru() {
             oops: 'Что-то пошло не так',
 			cycle_length: 'Укажите длину вашего цикла',
 			menstruation_length: 'Укажите длину месячных',
-			menstruated_at: 'Укажите начало менструального цикла',
+			menstruated_at: 'Укажите начало последнего менструального цикла',
 			bdate: 'Укажите вашу дату рождения',
 			day: 'День',
 			month: 'Месяц',
