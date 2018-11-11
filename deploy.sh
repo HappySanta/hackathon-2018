2aaf52aa6f997b07ba62fe4c40caf196a074983a
Иван Недзвецкий <stels-cs@yandex.ru>
2018-11-11 05:23:30 +0300

    Reis

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 ssh dp@web5.vkforms.ru 'cd /var/www/hack.w5.vkforms.ru && git pull origin master'
 ssh dp@web5.vkforms.ru 'cd /var/www/hack.w5.vkforms.ru && composer install -o --no-dev'
 ssh dp@web5.vkforms.ru 'cd /var/www/hack.w5.vkforms.ru && php artisan migrate --force'
+ssh dp@web5.vkforms.ru 'cd /var/www/hack.w5.vkforms.ru && php artisan queue:restart'
 cd frontend
 npm run deploy
 
